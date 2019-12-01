3466e1f0acc1cd0bae766fe3932e6da47c50acb4
zhaozeyu <584532318@qq.com>
2019-12-01 23:19:33 +0800

    添加vs2019的prism的基础工程文件

---
+++ Project/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace Project
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
